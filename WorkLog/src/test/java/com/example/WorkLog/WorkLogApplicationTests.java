f7d29fbe57c0de03e2a96ef0e475b2e7acdd4389
Ujjwal Dhakal <70379407+Ujjwal-D@users.noreply.github.com>
2024-11-02 06:31:57 +1000

    Initial commit for project WorkLog v1.0.0.0

---
+++ WorkLog/src/test/java/com/example/WorkLog/WorkLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
