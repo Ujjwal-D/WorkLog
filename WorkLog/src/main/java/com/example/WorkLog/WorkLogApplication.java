27fe29b6752eec00d071cb2d25a5ac1e3295a585
Ujjwal Dhakal <70379407+Ujjwal-D@users.noreply.github.com>
2024-11-29 05:44:56 +1000

    Security Introduced along and updated build files along with seeding

--- WorkLog/src/main/java/com/example/WorkLog/WorkLogApplication.java
+++ WorkLog/src/main/java/com/example/WorkLog/WorkLogApplication.java
@@ -1,7 +1,11 @@
 package com.example.WorkLog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.WorkLog.service.UserService;
 
 @SpringBootApplication
 public class WorkLogApplication {
@@ -9,5 +13,12 @@ public class WorkLogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkLogApplication.class, args);
 	}
+
+	@Bean
+    CommandLineRunner init(UserService userService) {
+        return args -> {
+            userService.registerUser("Admin", "admin@example.com", "admin123", "ROLE_ADMIN");
+        };
+    }
 
 }
